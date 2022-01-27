69183f5e17c738b3a413a7e3030358f76b5911b7
Alex Bedard-Reid <alexbedardreid@gmail.com>
2022-01-26 20:52:57 -0800

    Added Death Announcement to CharacterTileInteractionBase

--- Assets/Scripts/Player/Interactions/CharacterTileInteractionBase.cs
+++ Assets/Scripts/Player/Interactions/CharacterTileInteractionBase.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 public abstract class CharacterTileInteractionBase : MonoBehaviour, IInteractWithTile
 {
+    public static Action<CharacterTileInteractionBase> OnCharacterDied;
+    
     [SerializeField, Header("Trail Effects")]
     //Can the player move
     protected Material trailMaterial;
@@ -16,7 +20,9 @@ public abstract class CharacterTileInteractionBase : MonoBehaviour, IInteractWit
     protected void DestroyCharacter(in string cause, in Object killedObject = null)
     {
         Debug.Log($"{gameObject.name} was killed by {cause}", killedObject);
+        OnCharacterDied?.Invoke(this);
         Destroy(gameObject);
+        
     }
 
     //====================================================================================================================//
